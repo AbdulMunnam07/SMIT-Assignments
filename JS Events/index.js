cd2fcb4387d1ab3172ab628790ec3ffbbfacd063
AbdulMunnam07 <createrking40@gmail.com>
2024-05-26 22:21:22 +0500

    New Update

---
+++ JS Events/index.js
@@ -0,0 +1,13 @@
+                                // Events: link
+
+// function clicky() {
+//     alert("I'm Clicked")
+// };
+
+                                // Events: button
+
+// function focus() {
+//     document.getElementById(btn).style.backgroundColor = "red";
+//     alert("What the matter with you? There is no tomorrow");
+
+// }
