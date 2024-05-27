b78122bff51ee40a709a29ef9634c8dc48f401c6
AbdulMunnam07 <createrking40@gmail.com>
2024-05-27 11:17:18 +0500

    New Update

--- JS Events/index.js
+++ JS Events/index.js
@@ -4,10 +4,18 @@
 //     alert("I'm Clicked")
 // };
 
+
                                 // Events: button
 
 // function focus() {
 //     document.getElementById(btn).style.backgroundColor = "red";
 //     alert("What the matter with you? There is no tomorrow");
-
 // }
+
+
+                                // Events: mouse
+
+function para() {
+    let check = document.getElementById(Paragraph).innerHTML = "Hey good you get it done"
+    console.log("check");
+}
