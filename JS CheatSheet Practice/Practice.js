8d9c696ce99e9d80e5d73766c70caaa3595be995
AbdulMunnam07 <createrking40@gmail.com>
2024-07-02 12:42:53 +0500

    Check

---
+++ JS CheatSheet Practice/Practice.js
@@ -0,0 +1,48 @@
+//   1.  JavaScript Array
+
+var num = [1,2,3,4,6]
+
+//  We can acess value of the given array by this value
+// console.log(num[4]);
+// console.log(num.at(2));
+
+// TO add the value at the end
+// console.log(num.push(7));
+// console.log(num);
+
+// TO add the value at the start
+// console.log(num.unshift(10));
+// console.log(num);
+
+// TO remove the value at the end
+// console.log(num.pop());
+
+// TO remove the value at the start
+// console.log(num.shift());
+
+// To reverse array
+// console.log(num.reverse());
+
+// To fill all the array element with specific number
+// console.log(num.fill(8));
+
+// To check if the array has specific number or not, if it include it will return true otherwise false
+// console.log(num.includes(4));
+
+// Map function (Har 1 banday k pass jao or uska roll no. note kro)
+// console.log(num.map( item => 2*item));
+
+// Filter Function (JO test ma pass hain un logo ko alag krdo Or jo nahi pass unhay alag)
+// console.log(num.filter(item => item > 2));
+
+// To find element first match
+// console.log(num.find(item => item > 2));
+
+// To determine the index of the specific element
+// console.log(num.indexOf(4));
+
+// console.log(num.findIndex(item => item == 4));
+
+// console.log(num.reduce( (pre, curr) => pre + curr) );
+
+
